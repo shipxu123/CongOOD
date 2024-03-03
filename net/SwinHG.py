e167f76bb113c489574926f6db9c4f216e242038
shipxu123 <89835119+shipxu123@users.noreply.github.com>
2024-03-03 18:47:09 +0800

    Update SwinHG.py

--- net/SwinHG.py
+++ net/SwinHG.py
@@ -1069,11 +1069,11 @@ class SwinUPer(nn.Module):
         h = graph.ndata['h']
         x = h['Gcell']
 
-        #record xs, by Peng
-        self.record(x.cpu())
-
         rearrange_x = Rearrange('(b h w) (p1 p2 c) -> b c (h p1) (w p2)', p1 = 4, p2 = 4, h=64, w=64)
         x = rearrange_x(x)
+
+        #record xs, by Peng
+        self.record(x.cpu())
 
         #tmp = x[:, 1:2, :, :] * x[:, 3:5, :, :]
         feats = self.vit(graph)
